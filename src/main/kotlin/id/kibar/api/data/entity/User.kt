ae8642d2a8c94802f3a044525846fa227324147d
Adrian Spanu <adri@nspanu.ca>
2017-09-25 16:57:10 +0700

    Creating the very first version of the Api endpoints so we can start testing the React app against something.

--- src/main/kotlin/id/kibar/api/data/entity/User.kt
+++ src/main/kotlin/id/kibar/api/data/entity/User.kt
@@ -1,9 +1,9 @@
 package id.kibar.api.data.entity
 
 data class User(
-    val id: Int,
+    var id: Int = 0,
     val email: String,
-    val password: String,
     val firstName: String,
-    val lastName: String
+    val lastName: String,
+    var sub: String = "N/A" // The unique Id provided by google from the Oauth process
 )
